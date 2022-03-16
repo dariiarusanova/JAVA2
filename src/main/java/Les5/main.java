4ae35fee9a2bf7da5d1b2f2996a45f78d9836dcc
dariiarusanova <dariiarusanova@gmail.com>
2022-03-16 23:52:29 +0300

    Дз5

--- src/main/java/Les5/main.java
+++ src/main/java/Les5/main.java
@@ -9,26 +9,25 @@ import java.util.ArrayList;
 public class main {
     public static void main(String[] args) {
         // 1. Реализовать сохранение данных в csv файл;
-       try {
+        try {
             FileOutputStream SaveFileLess5 = new FileOutputStream("Счастье внутри берегущего.csv", true);
             byte[] Databytes = "Счастье внутри берегущего".getBytes(StandardCharsets.UTF_8);
             SaveFileLess5.write(Databytes);
             SaveFileLess5.close();
-        } catch (Exception exception){
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
 
 //2. Реализовать загрузку данных из csv файла. Файл читается целиком.
- // ДЛЯ ФАЙЛА СЧАСТЬЕ ВНУТРИ:
- try {
-     FileInputStream LookFileLess5 = new FileInputStream("Счастье внутри берегущего.csv");
-     byte[] bytes = new byte[LookFileLess5.available()] ;
-     LookFileLess5.read(bytes);
-     System.out.println(new String(bytes));
-        } catch (Exception ex){
+        // ДЛЯ ФАЙЛА СЧАСТЬЕ ВНУТРИ:
+        try {
+            FileInputStream LookFileLess5 = new FileInputStream("Счастье внутри берегущего.csv");
+            byte[] bytes = new byte[LookFileLess5.available()];
+            LookFileLess5.read(bytes);
+            System.out.println(new String(bytes));
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-
 
 
         //ДЛЯ ФАЙЛА APPDATA:
@@ -38,20 +37,28 @@ public class main {
             byte[] Databytess = "AppData".getBytes(StandardCharsets.UTF_8);
             SaveFile.write(Databytess);
             SaveFile.close();
-        } catch (Exception exception){
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
-               try {
+        try {
             FileInputStream READAPDATA = new FileInputStream("Файл из AppData.csv");
             ArrayList<Byte> read = new ArrayList<>();
             final int EOF = -1;
             int temp = 0;
-            while ((temp = READAPDATA.read()) != EOF){
+            while ((temp = READAPDATA.read()) != EOF) {
                 read.add((byte) temp);
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+        System.out.println(newData());
+    }
+    public static AppData newData () {
+        AppData appData = new AppData();
+        String[] headers = new String[]{"title 1", "title 2", "title 3"};
+        appData.init(headers);
+
+        return appData;
     }
 
 }
